b504dea418cca8674d32cec2eaedff5a45704df6
Peter <peterc@optum.com>
2021-05-13 12:28:08 +0100

    testing with two microfrontends

---
+++ page-a/webpack.config.js
@@ -0,0 +1,18 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "acme",
+    projectName: "page-a",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port: 8002,
+    },
+  });
+};
---
+++ page-b/webpack.config.js
@@ -0,0 +1,18 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "acme",
+    projectName: "page-b",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port: 8003,
+    },
+  });
+};
